e0e2e261f9b97df950f12add7be936a6e11e0afe
Tran Cao Dua <duatc@smartosc.com>
2024-01-19 17:43:49 +0700

    Section 5: Vue: Behind the Scenes

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,78 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.$refs.userText.value;
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('create()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
+});
+
+app.mount('#app');
+
+setTimeout(function () {
+  app.unmount();
+}, 3000)
+
+const app2 = Vue.createApp({
+  template: `<p>{{favoriteMeal}}</p>`,
+  data() {
+    return {
+      favoriteMeal: 'Pizza!'
+    }
+  }
+})
+
+app2.mount('#app2');
+
+//...
+const data = {
+  message: 'Hello',
+  longMessage: 'Hello! World!'
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World';
+    }
+    target.message = value;
+  }
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = "Hello!!!";
+
+console.log(proxy.longMessage);
