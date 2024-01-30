4cb3e1761451ccd4cf8a103ddfe150f8e5d8883f
Tran Cao Dua <duatc@smartosc.com>
2024-01-30 12:43:06 +0700

    Section 16: Main Project: Find a Coach Web App

---
+++ main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,7 @@
+<template>
+    Details for coach
+    <router-view></router-view>
+    <router-link to="/coaches/c1/contact">
+        Contact
+    </router-link>
+</template>
---
+++ main-prj-01-starting-setup/src/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import NotFound from './pages/NotFound.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistation from './pages/coaches/CoachRegistation.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceive from './pages/requests/RequestsReceive.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: CoachesList },
+        {
+            path: '/coaches/:id',
+            component: CoachDetail,
+            children: [
+                { path: 'contact', component: ContactCoach }  // coaches/c1/contact
+            ]
+        },
+        { path: '/register', component: CoachRegistation },
+        { path: '/request', component: RequestsReceive },
+        { path: '/:notFound(.*)', component: NotFound },
+    ]
+
+});
+
+export default router;
