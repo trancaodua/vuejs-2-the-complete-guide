bd5230b8fbedb73832cb4232bef8ea3c07a86613
trancaodua <trancaodua199@gmail.com>
2024-01-31 21:42:12 +0700

    Add file

---
+++ auth-01-starting-project/src/store/modules/auth/actions.js
@@ -0,0 +1,33 @@
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCyi0M0KtFKUgvNUa4MQFIUFCYAeENMC3I',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
